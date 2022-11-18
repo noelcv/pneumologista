fe91055a345e4608a7b07865ede31372e4a82fd7
noelcv <79803356+noelcv@users.noreply.github.com>
2022-11-18 19:22:27 +0100

    add breakpoints

--- pneumologista/src/pages/Homepage.tsx
+++ pneumologista/src/pages/Homepage.tsx
@@ -3,7 +3,8 @@ import DoctorCard from '../components/DoctorCard'
 
 const Homepage: FunctionComponent = () => {
   const bioAntonio = 'Especialista em Pneumologia, inscrito no Colégio da Especialidade da Ordem dos Médicos, membro da Sociedade Portuguesa de Pneumologia e da European Respiratory Society.'
-  const cvAntonio = ['2019: Diploma Europeu em Medicina Respiratória do Adulto (European Respiratory Society)',
+  const cvAntonio = [
+  '2019: Diploma Europeu em Medicina Respiratória do Adulto (European Respiratory Society)',
   'Internato de Formação Específica de Pneumologia no Centro Hospitalar e Universitário de Coimbra, E.P.E.',
   'Formação em Medicina Intensiva na Unidade de Cuidados Intensivos Respiratórios do Centro Hospitalar Lisboa Norte, E.P.E.', 
   'Mestrado Integrado em Medicina na Faculdade de Ciências da Saúde - UBI.', 
@@ -12,16 +13,18 @@ const Homepage: FunctionComponent = () => {
   const callAntonio = 'https://maislusiadas.pt/Portal/DoctorDetail?PractitionerId=2353'
   const bioManuel = 'Especialista em Pneumologia, membro da Sociedade Portuguesa de Pneumologia e da European Respiratory Society'
   
-  const cvManuel = ['Desde 2020 - Pneumologista, Clínica CUF S. Domingos de Rana', 'Desde 2020 - Pneumologista, Hospital de Cascais', 
+  const cvManuel = [
+  'Desde 2020 - Pneumologista, Clínica CUF S. Domingos de Rana', 'Desde 2020 - Pneumologista, Hospital de Cascais', 
   '2018-2020 - Colaborador como professor convidado na formação pré-graduada dos alunos da Licenciatura em Cardiopneumologia da Escola Superior de Saúde da Cruz Vermelha Portuguesa',
   '2015-2020 - Colaborador na formação pré-graduada dos alunos do 3º, 4º e 6º ano do Mestrado Integrado em Medicina da Faculdade de Medicina da Universidade de Lisboa',
   '2015-2020 - Internato Complementar de Pneumologia, Centro Hospitalar Universitário Lisboa Norte', 
-  '2014 - Internato do Ano Comum, Centro Hospitalar de Vila Nova de Gaia/Espinho', '2012 - Mestrado Integrado em Medicina, Faculdade de Ciências da Saúde, Universidade da Beira Interior']
+  '2014 - Internato do Ano Comum, Centro Hospitalar de Vila Nova de Gaia/Espinho', 
+  '2012 - Mestrado Integrado em Medicina, Faculdade de Ciências da Saúde, Universidade da Beira Interior']
   const callManuel = 'https://www.saudecuf.pt/mycuf/login/login/appointment/consult?q=fHx8bWFudWVsfGZlcm5hbmRlc3w1NDgxOA==&apptype=R&_ga=2.124050316.709124691.1668533670-979208369.1668533670'
   
   
   return (
-      <div className="flex justify-between ml-5 -mr-3 md:mx-auto flex-col sm:flex-row md:grid md:grid-flow-col md:gap-32 xl:gap-96">
+      <div className="flex justify-between ml-5 -mr-3 md:mx-auto flex-col sm:flex-row md:grid md:grid-flow-col sm:gap-12 md:gap-32 xl:gap-96">
           <div>
             <DoctorCard name="Antonio Fernandes" image="/antonio.jpeg" location="Porto, Braga, Viana" bio={bioAntonio} cv={cvAntonio} callLink={callAntonio}/>
           </div>
